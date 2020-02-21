0e2cdcf2890f274f86edc0946203fd4883071c83
Dustin Johnson <dustinjohnson@fastmail.fm>
2020-02-21 10:56:31 -0500

    Added createdAt, lastLoggedIn fields to project users.

--- api/models/projectUserModel.js
+++ api/models/projectUserModel.js
@@ -18,6 +18,13 @@ const projectUserSchema = mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Project',
         required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    lastLoggedIn: {
+        type: Date
     }
 });
 
